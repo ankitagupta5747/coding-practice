1add726c0f70f0e8a1e415e7336ea217e4d45b02
Ankita Gupta <ankitagupta5747@gmail.com>
2026-09-26 20:29:19 +0530

    bracket pair solution

---
+++ leetcode/bracket pair.py
@@ -0,0 +1,30 @@
+class Solution:
+    def evaluate(self, s, knowledge):
+        d = {}
+
+        for key, value in knowledge:
+            d[key] = value
+
+        result = ""
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = i + 1
+
+                while s[j] != ')':
+                    j += 1
+
+                key = s[i + 1:j]
+
+                if key in d:
+                    result += d[key]
+                else:
+                    result += "?"
+
+                i = j + 1
+            else:
+                result += s[i]
+                i += 1
+
+        return result
